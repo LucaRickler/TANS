61be9257b2a3850d8263af6f5b58eefde90772da
Luca Rickler <luca.rickler@gmail.com>
2016-11-04 17:40:10 +0100

    Particle.h

--- Src/Particle.h
+++ Src/Particle.h
@@ -27,16 +27,6 @@ class Particle : public TObject{
 		Particle(PType ptype, double energy, const Vector3D& direction, bool primary = false); //(const Vector3D&, double theta); // nell'implementazione, devi omettere ""= false", altrimenti non compila
 		//void PFeatures(PType ptype, double penergy, bool pprimary); //??? A cosa serve? Senza reference non puoi passare dati all'esterno
 
-		void Propagate(); //Trasporto della particella
-		/*
-		* static Vector3D delta_pos; (Codice vero, devi scriverlo così)
-		*	old_position = position
-		*	position = ...
-		* delta_pos += position - old_position (ci sono gli operatori di somma e differenza per Vector3D, quindi puoi fare c=a+-b)
-		* if(delta_pos > libero cammino assorb)
-		* 	assorbi (non te ne preoccupare adesso, lo faremo più tardi. Sostituiscilo con solo un ";", fidati)
-		*/
-
 		bool Divide(Particle* p1, Particle* p2); //vedi gli appunti che ti avevo scritto, questo deve:
 		/* if(soglia)
 		*  	if(PGAMMA)
@@ -49,11 +39,32 @@ class Particle : public TObject{
 		*	(else) (non serve scriverlo, è per farti capire il concetto. Senza il codice è più elegante)
 		* 	return false
 		*/
+
 		//Nel codice chiamante:
 		/*
 		* for (Particle *p in all_particles)
 		* 	Particle *p1, *p2
 		* 	if(p->Divide(p1,p2))
+		*			all_particles.remove(p)
+		*			delete p
+		*/
+
+		bool Propagate(); //Trasporto della particella
+		/*
+		* static Vector3D delta_pos; (Codice vero, devi scriverlo così)
+		*	old_position = position
+		*	position = ...
+		* delta_pos += position - old_position (ci sono gli operatori di somma e differenza per Vector3D, quindi puoi fare c=a+-b)
+		* if(delta_pos > libero cammino assorb)
+		* 	return false
+		*	(else) (non serve scriverlo, è per farti capire il concetto. Senza il codice è più elegante)
+		* 	return false
+		*/
+
+		//Nel codice chiamante:
+		/*
+		* for (Particle *p in all_particles)
+		* 	if(!p->Propagate()) (!true == false, !false == true. ! è la negazione booleana)
 		*			all_particles.remove(p)
 		*			delete p
 		*/
