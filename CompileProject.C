6d4a59d04c2fa68997a94884e57fe5114fa5c072
Luca Rickler <luca.rickler@gmail.com>
2016-12-14 16:01:50 +0100

    Relazione

--- CompileProject.C
+++ CompileProject.C
@@ -2,27 +2,8 @@
 #include <TROOT.h>
 #endif
 
-enum Tests {
-  ParticleTest = 0,
-  BSEnergyTest
-};
-
 void CompileSimulation() {
   gROOT->ProcessLine(".L Src/Vector3D.cpp+");
   gROOT->ProcessLine(".L Src/Particle.cpp+");
   gROOT->ProcessLine(".L Src/Simulation.C+");
-}
-
-void CompileTest(Tests test_number) {
-  switch (test_number) {
-    case ParticleTest:
-      gROOT->ProcessLine(".L Src/Vector3D.cpp+");
-      gROOT->ProcessLine(".L Src/Particle.cpp+");
-      gROOT->ProcessLine(".L UnitTests/Particle_test.C+");
-      break;
-    case BSEnergyTest:
-      gROOT->ProcessLine(".L Src/Vector3D.cpp+");
-      gROOT->ProcessLine(".L Src/Particle.cpp+");
-      gROOT->ProcessLine(".L UnitTests/BSEnergy_test.C+");
-  }
 }
